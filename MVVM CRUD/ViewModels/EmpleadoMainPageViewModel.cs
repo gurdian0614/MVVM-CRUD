df24da5304d3ff7130e00c5360cedafe27c4a686
gurdian0614 <ivan.gurdian@outlook.com>
2024-03-04 19:26:31 -0600

    Agregando view model

--- MVVM CRUD/ViewModels/EmpleadoMainPageViewModel.cs
+++ MVVM CRUD/ViewModels/EmpleadoMainPageViewModel.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MVVM_CRUD.Models;
+using MVVM_CRUD.Services;
+using System.Collections.ObjectModel;
 
 namespace MVVM_CRUD.ViewModels
 {
-    internal class EmpleadoMainPageViewModel
+    internal partial class EmpleadoMainPageViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private ObservableCollection<Empleado> empleadoCollection = new ObservableCollection<Empleado>();
+
+        private readonly EmpleadoService _empleadoService;
+
+        public EmpleadoMainPageViewModel()
+        {
+            _empleadoService = new EmpleadoService();
+        }
+
+        public void GetAll()
+        {
+            var getAll = _empleadoService.GetAll();
+
+            if (getAll?.Count > 0)
+            {
+                EmpleadoCollection.Clear();
+                foreach (var empleado in getAll)
+                {
+                    EmpleadoCollection.Add(empleado);
+                }
+            }
+        }
     }
 }
