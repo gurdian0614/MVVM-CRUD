399db4a3534398a668d771293ee20308c6a3f612
gurdian0614 <ivan.gurdian@outlook.com>
2024-03-04 19:14:08 -0600

    Configurando base de datos

--- MVVM CRUD/Services/EmpleadoService.cs
+++ MVVM CRUD/Services/EmpleadoService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MVVM_CRUD.Models;
+using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,32 @@ namespace MVVM_CRUD.Services
 {
     internal class EmpleadoService
     {
+        private readonly SQLiteConnection _dbConnection;
+
+        public EmpleadoService() {
+            string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Empleado.db");
+            _dbConnection = new SQLiteConnection(dbPath);
+            _dbConnection.CreateTable<Empleado>();
+        }
+
+        public List<Empleado> GetAll() {
+            var res = _dbConnection.Table<Empleado>().ToList();
+            return res;
+        }
+
+        public int Insert(Empleado empleado)
+        {
+            return _dbConnection.Insert(empleado);
+        }
+
+        public int Update(Empleado empleado)
+        {
+            return _dbConnection.Update(empleado);
+        }
+
+        public int Delete(Empleado empleado)
+        {
+            return _dbConnection.Delete(empleado);
+        }
     }
 }
