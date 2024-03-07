a91b5acf3ab6215adbca612893134fbe2aa61966
gurdian0614 <ivan.gurdian@outlook.com>
2024-03-06 19:19:01 -0600

    Agregando logica

--- MVVM CRUD/ViewModels/AddEmpleadoPageViewModel.cs
+++ MVVM CRUD/ViewModels/AddEmpleadoPageViewModel.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MVVM_CRUD.Models;
+using MVVM_CRUD.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,54 @@ using System.Threading.Tasks;
 
 namespace MVVM_CRUD.ViewModels
 {
-    internal class AddEmpleadoPageViewModel
+    public partial class AddEmpleadoPageViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private int id;
+
+        [ObservableProperty]
+        private string nombre;
+
+        [ObservableProperty]
+        private string email;
+
+        [ObservableProperty]
+        private string direccion;
+
+        private readonly EmpleadoService _empleadoService;
+
+        public AddEmpleadoPageViewModel()
+        {
+            _empleadoService = new EmpleadoService();
+        }
+
+        public AddEmpleadoPageViewModel(Empleado empleado)
+        {
+            Nombre = empleado.Nombre;
+            Email = empleado.Email;
+            Direccion = empleado.Direccion;
+            Id = empleado.Id;
+            _empleadoService = new EmpleadoService();
+        }
+
+        [RelayCommand]
+        private async Task AddUpdate()
+        {
+            Empleado empleado = new Empleado {
+                Nombre = Nombre,
+                Email = Email,
+                Direccion = Direccion,
+                Id = Id
+            };
+
+            if (Id == 0)
+            {
+                _empleadoService.Insert(empleado);
+            } else
+            {
+                _empleadoService.Update(empleado);
+            }
+            await App.Current.MainPage.Navigation.PopAsync();
+        }
     }
 }
--- MVVM CRUD/ViewModels/EmpleadoMainPageViewModel.cs
+++ MVVM CRUD/ViewModels/EmpleadoMainPageViewModel.cs
@@ -1,11 +1,13 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using MVVM_CRUD.Models;
 using MVVM_CRUD.Services;
+using MVVM_CRUD.Views;
 using System.Collections.ObjectModel;
 
 namespace MVVM_CRUD.ViewModels
 {
-    internal partial class EmpleadoMainPageViewModel : ObservableObject
+    public partial class EmpleadoMainPageViewModel : ObservableObject
     {
         [ObservableProperty]
         private ObservableCollection<Empleado> empleadoCollection = new ObservableCollection<Empleado>();
@@ -28,6 +30,32 @@ namespace MVVM_CRUD.ViewModels
                 {
                     EmpleadoCollection.Add(empleado);
                 }
+            }
+        }
+
+        [RelayCommand]
+        private async Task GoToAddEmpleadoPage()
+        {
+            await App.Current.MainPage.Navigation.PushAsync(new AddEmpleadoPage());
+        }
+
+        [RelayCommand]
+        private async Task SelectEmpleado(Empleado empleado)
+        {
+            string res = await App.Current.MainPage.DisplayActionSheet("Operación", "Cancelar", null, "Actualizar", "Eliminar");
+
+            switch (res)
+            {
+                case "Actualizar":
+                    await App.Current.MainPage.Navigation.PushAsync(new AddEmpleadoPage(empleado));
+                    break;
+                case "Eliminar":
+                    int del = _empleadoService.Delete(empleado);
+                    if (del > 0)
+                    {
+                        EmpleadoCollection.Remove(empleado);
+                    }
+                    break;
             }
         }
     }
