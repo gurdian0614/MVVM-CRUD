f9821f225f0fc2ede4f015e7142cc298725f8ca4
gurdian0614 <ivan.gurdian@outlook.com>
2024-03-06 19:19:12 -0600

    Construyendo front

--- MVVM CRUD/Views/AddEmpleadoPage.xaml.cs
+++ MVVM CRUD/Views/AddEmpleadoPage.xaml.cs
@@ -1,9 +1,22 @@
+using MVVM_CRUD.Models;
+using MVVM_CRUD.ViewModels;
+
 namespace MVVM_CRUD.Views;
 
 public partial class AddEmpleadoPage : ContentPage
 {
+	private AddEmpleadoPageViewModel _viewModel;
 	public AddEmpleadoPage()
 	{
 		InitializeComponent();
+		_viewModel = new AddEmpleadoPageViewModel();
+		this.BindingContext = _viewModel;
+	}
+
+	public AddEmpleadoPage(Empleado empleado)
+	{
+		InitializeComponent();
+		_viewModel = new AddEmpleadoPageViewModel(empleado);
+		this.BindingContext = _viewModel;
 	}
 }
--- MVVM CRUD/Views/EmpleadoMainPage.xaml.cs
+++ MVVM CRUD/Views/EmpleadoMainPage.xaml.cs
@@ -1,9 +1,20 @@
+using MVVM_CRUD.ViewModels;
+
 namespace MVVM_CRUD.Views;
 
 public partial class EmpleadoMainPage : ContentPage
 {
+	private EmpleadoMainPageViewModel _viewModel;
 	public EmpleadoMainPage()
 	{
 		InitializeComponent();
+		_viewModel = new EmpleadoMainPageViewModel();
+		this.BindingContext = _viewModel;
 	}
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+		_viewModel.GetAll();
+    }
 }
